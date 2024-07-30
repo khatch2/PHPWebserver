45438c49d6f003a3cedf0b58f09f8c5d16430dd4
khatch2_name <20730681+khatch2@users.noreply.github.com>
2024-07-30 17:54:11 +0200

    fifth composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '24khsh/php-webserver',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '9bee3bed65fd15e691ef16c1ce81371255a151d6',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '24khsh/php-webserver' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '9bee3bed65fd15e691ef16c1ce81371255a151d6',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
